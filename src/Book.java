278a6f738903777d8523a3686838f8373848edd2
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-07-03 14:53:08 +0500

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,16 +1,11 @@
-public class Book {
-    private String title;
+public class Book extends LibraryItem {
     private String author;
     private boolean isAvailable;
 
-    public Book(String title, String author) {
-        this.title = title;
+    public Book(String name, String id, String author) {
+        super(name, id);
         this.author = author;
         this.isAvailable = true;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public String getAuthor() {
@@ -23,5 +18,13 @@ public class Book {
 
     public void setAvailable(boolean isAvailable) {
         this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Book ID: " + getId());
+        System.out.println("Title: " + getName());
+        System.out.println("Author: " + author);
+        System.out.println("Available: " + isAvailable);
     }
 }
