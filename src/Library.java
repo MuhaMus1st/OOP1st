f0636b091a4a07308b82a742e79437debd01b9e9
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-07-03 14:53:19 +0500

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -1,72 +1,74 @@
-import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Library {
-    private List<Book> books = new ArrayList();
+    private List<LibraryItem> items;
 
     public Library() {
+        this.items = new ArrayList<>();
     }
 
-    public void addBook(Book book) {
-        this.books.add(book);
+    public void addItem(LibraryItem item) {
+        items.add(item);
     }
 
-    public boolean borrowBook(String title) {
-        Iterator var2 = this.books.iterator();
-
-        Book book;
-        do {
-            if (!var2.hasNext()) {
-                System.out.println("Sorry, the book '" + title + "' is not available");
-                return false;
+    public boolean borrowBook(String id) {
+        for (LibraryItem item : items) {
+            if (item instanceof Book && item.getId().equalsIgnoreCase(id)) {
+                Book book = (Book) item;
+                if (book.isAvailable()) {
+                    book.setAvailable(false);
+                    System.out.println("You have successfully borrowed \"" + book.getName() + "\".");
+                    return true;
+                } else {
+                    System.out.println("Sorry, the book \"" + book.getName() + "\" is not available.");
+                    return false;
+                }
             }
-
-            book = (Book)var2.next();
-        } while(!book.getTitle().equalsIgnoreCase(title) || !book.isAvailable());
-
-        book.setAvailable(false);
-        System.out.println("You have successfully borrowed the book '" + title + "'");
-        return true;
+        }
+        System.out.println("Sorry, the book with ID \"" + id + "\" is not found.");
+        return false;
     }
 
-    public boolean returnBook(String title) {
-        Iterator var2 = this.books.iterator();
-
-        Book book;
-        do {
-            if (!var2.hasNext()) {
-                System.out.println("Sorry, the book '" + title + "' is not from this library");
-                return false;
+    public boolean returnBook(String id) {
+        for (LibraryItem item : items) {
+            if (item instanceof Book && item.getId().equalsIgnoreCase(id)) {
+                Book book = (Book) item;
+                if (!book.isAvailable()) {
+                    book.setAvailable(true);
+                    System.out.println("You have successfully returned \"" + book.getName() + "\".");
+                    return true;
+                } else {
+                    System.out.println("The book \"" + book.getName() + "\" was not borrowed.");
+                    return false;
+                }
             }
-
-            book = (Book)var2.next();
-        } while(!book.getTitle().equalsIgnoreCase(title) || book.isAvailable());
-
-        book.setAvailable(true);
-        System.out.println("You have successfully returned '" + title + "'");
-        return true;
+        }
+        System.out.println("Sorry, the book with ID \"" + id + "\" is not found.");
+        return false;
     }
 
     public void displayAvailableBooks() {
         System.out.println("Available books:");
         boolean anyAvailable = false;
-        Iterator var2 = this.books.iterator();
-
-        while(var2.hasNext()) {
-            Book book = (Book)var2.next();
-            if (book.isAvailable()) {
-                PrintStream var10000 = System.out;
-                String var10001 = book.getTitle();
-                var10000.println("Title: " + var10001 + ", Author: " + book.getAuthor());
-                anyAvailable = true;
+        for (LibraryItem item : items) {
+            if (item instanceof Book) {
+                Book book = (Book) item;
+                if (book.isAvailable()) {
+                    System.out.println("ID: " + book.getId() + ", Title: " + book.getName() + ", Author: " + book.getAuthor());
+                    anyAvailable = true;
+                }
             }
         }
-
         if (!anyAvailable) {
-            System.out.println("No books are currently available.But I can give you one if you win me on 1 v 1 ");
+            System.out.println("No books are currently available.");
         }
+    }
 
+    public void displayAllItems() {
+        for (LibraryItem item : items) {
+            item.displayDetails();
+            System.out.println();
+        }
     }
 }
